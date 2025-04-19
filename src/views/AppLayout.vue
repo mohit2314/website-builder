7d1699ea72f22a06aad7ddfe614122aee6db16fc
mohit2314 <mohitpatel2314@gmail.com>
2025-04-19 13:29:33 +0530

    App layout

---
+++ src/views/AppLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <LeftNavbar />
+  <TopNavbar />
+</template>
+
+<script setup lang="ts">
+import LeftNavbar from '@/components/LeftNavbar.vue'
+import TopNavbar from '@/components/TopNavbar.vue'
+</script>
+
+<style></style>
